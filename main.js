2a64195e5f126a1549342b6297dd0735604450d0
gsbae <48188188+gsbae@users.noreply.github.com>
2020-03-25 23:13:26 -0400

    6ab

--- main.js
+++ main.js
@@ -1,23 +1,245 @@
-var i = 0;
 
+function createBun(name, cost, image, description, glaze, quantity, type){
+    this.name = name;
+    this.cost = cost;
+    this.image = image;
+    this.description = description;
+    this.glaze = glaze;
+    this.quantity = quantity;
+    this.type = type;
+}
+
+function cartItem(name, glaze, quantity, cost){
+    this.name = name;
+    this.glaze = glaze;
+    this.quantity = quantity;
+    this.cost = cost;
+}
+
+/*keep track of items in cart */
 function addToCart() {
+    /*update button to notify that item was added*/
     document.getElementById("cart").innerHTML = "ADDED TO CART!";
     setTimeout(
         function(){
             document.getElementById("cart").innerHTML = "ADD TO CART!";
         }, 2000
     );
-    document.getElementById('inc').value = ++i;
+
+    /*increment number of items in cart
+    var count = Number(document.getElementById('inc').value);
+    count += 1;
+    document.getElementById('inc').value = count;
+    sessionStorage.setItem("count", JSON.stringify(count));
+`*/
+
+    /*get bun of current page*/
+    var bun = JSON.parse(localStorage.getItem("buns"));
+    selectGlaze(bun);
+    selectQty(bun);
+
+    /*compute cost of buns*/
+    const cost = bun.cost*bun.quantity
+    /*new cartItem for cart list*/
+    var addedItem = new cartItem(bun.name, bun.glaze, bun.quantity, Number(cost));
+    var cartItems = JSON.parse(sessionStorage.getItem("cart"));
+
+    if (cartItems === null) {
+        cartItems = [];
+        sessionStorage.setItem("cart", JSON.stringify(cartItems));
+    }
+    cartItems.push(addedItem);
+    sessionStorage.setItem("cart", JSON.stringify(cartItems));
+    document.getElementById('inc').value = cartItems.length;
 }
 
-/*
-function selectGlaze(){
-    document.getElementByClassName("option").style.backgroundColor = "#2E1900";
-    document.getElementByClassName("option").style.color = "white";
-}*/
-
-function deleteRow(item) {
-    var row = item.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-    document.getElementById('inc').value = --i;
+/* for selecting the glaze and quantity options in the product detail page*/
+function selectGlaze(item){
+    var glaze = document.getElementById("glazeOptions");
+    glaze.options[glaze.options.selectedIndex].selected = true;
+    var glazeText = glaze.options[glaze.selectedIndex].text;
+    item.glaze = glazeText;
 }
+
+function selectQty(item){
+    var qty = document.getElementById("qtyOptions");
+    qty.options[qty.options.selectedIndex].selected = true;
+    var qtyText = qty.options[qty.selectedIndex].text;
+    item.quantity = qtyText;
+    adjCost(item);
+}
+
+
+function matchedID(item){
+    if (item === "Original"){
+        var original = new createBun("Original Cinnamon Bun", 3.00, "images/original.png", 
+    "Our classic original cinnamon bun is a fan favorite. \
+    You can never go wrong with the original as it suits almost every taste \
+    palette out there.", "sugarmilk", 1, "Original");
+        return original;
+    } 
+    else if (item === "Blackberry"){
+        var blackberry = new createBun("Blackberry Bun", 3.75,"images/blackberry.png",
+    "Soft from-scratch rolls filled with brown sugar, \
+    cinnamon, and fresh blackberries, topped with a glaze of your choice. \
+    Our blackberry buns are a summer favorite to start of your day!", 
+    "sugarmilk", 1, "Blackberry");
+        return blackberry;
+    }
+    else if (item === "Pumpkin"){
+        var pumpkinSpice = new createBun("Pumpkin Spice Bun", 4.00, "images/pumpkin-spice.png",
+    "Our pumpkin spice buns are a fall favorite, perfect \
+    for a cozy day paired with a warm cup of coffee. The pumpkin spice \
+    and flavor make this bun the seasonal favorite that it is.", "sugarmilk", 
+    1, "Pumpkin");
+        return pumpkinSpice;
+    }
+    else if (item === "Caramel"){
+        var caramelPecan = new createBun("Caramel Pecan Bun", 3.75, "images/caramel-pecan.png",
+    "Our caramel pecan bun is filled with ooey gooey caramel \
+    goodness with crushed pecans sprinkled on top for the perfect contrast of \
+    crunchy and velvety textures! This bun is perfect for when you need a little\
+    extra sweetness in your life", "sugarmilk", 1, "Caramel");
+        return caramelPecan;
+    }
+    else if (item === "Walnut"){
+        var walnut = new createBun("Walnut Bun", 3.75, "images/walnut.png", 
+    "Everybody goes nuts for our walnut bun! \
+    Its most popular glaze pairing is our double chocolate glaze\
+    as it adds a perfect kick to the nutty flavor of our walnut bun.",
+    "sugarmilk", 1, "Walnut");
+        return walnut;
+    }
+    else if (item === "Gluten"){
+        var glutenFree = new createBun("Gluten Free Bun", 3.50, "images/gluten-free.png",
+    "We aim to make our buns accessible to everyone, \
+    and that includes our friends with any dietary restrictions. That's \
+    why we chose to create a gluten-free option of our original cinnamon bun.\
+    Of course, we have plans to expand and provide more flavors to our gluten-free\
+    friends, but we hope you're able eo enjoy your experience with this one for now!",
+    "sugarmilk", 1, "Gluten");
+        return glutenFree;
+    }
+}
+
+/*format number to currency*/
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
+
+/*store the selected bun type into local storage*/
+function clickBun(item){
+    var bun = matchedID(item);
+    localStorage.setItem("buns", JSON.stringify(bun));
+}
+
+
+function loadProdPage(){
+    /*get stored bun */
+    var cartItems = JSON.parse(sessionStorage.getItem("cart"));
+    document.getElementById('inc').value = cartItems.length;
+    var bun = JSON.parse(localStorage.getItem("buns"));
+    
+    /*update page for specific bun*/
+    document.getElementById("prodName").textContent = bun.name;
+    document.getElementById("prodCost").textContent = formatter.format(bun.cost);
+    document.getElementById("description").textContent = bun.description;
+    document.getElementById("image1").setAttribute("src", bun.image);
+
+}
+
+function loadCartPage(){
+
+    var cartItems = JSON.parse(sessionStorage.getItem("cart"));
+
+    var total = 0;
+
+    for (var i = 0; i < cartItems.length; i++){
+        var item = cartItems[i];
+        console.log(item);
+        var table = document.getElementsByClassName("itemsInCart")[0];
+     
+        var row = table.insertRow(1);
+
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = item.name;
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = '<select id="glazeOptions">\
+                        <option id="sugar-milk">sugar-milk</option>\
+                        <option id="vanilla-milk">vanilla-milk</option>\
+                        <option id="double chocolate">double chocolate</option>\
+                        <option id="strawberry">strawberry</option>\
+                    </select>';
+        var glaze = document.getElementById(item.glaze);
+        glaze.selected = true;
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = '<select id="qtyOptions"> \
+                        <option id="1" >1</option>\
+                        <option id="3">3</option>\
+                        <option id="6">6</option>\
+                        <option id="12">12</option>\
+                    </select>';
+        const quant1 = Number(item.cost)/Number(item.quantity);
+        item.cost = quant1*item.quantity;
+        var qty = document.getElementById(item.quantity);
+        qty.selected = true;
+        var cell4 = row.insertCell(3);
+        cell4.id = "itemCost";
+        cell4.innerHTML = formatter.format(item.cost);
+        var cell5 = row.insertCell(4);
+        cell5.innerHTML = '<input type="button" value="x" onclick="deleteRow(this)"/>';
+        total += item.cost;
+    }
+    document.getElementById("sub").textContent = formatter.format(total);
+    var tax = total*.025;
+    document.getElementById("tax").textContent = formatter.format(tax);
+    document.getElementById("total").textContent = formatter.format(total+5+tax);
+    sessionStorage.setItem("cart", JSON.stringify(cartItems));
+
+    if (cartItems.length == 0){
+        var table = document.getElementsByClassName("itemsInCart")[0];
+     
+        var row = table.insertRow(1);
+        row.id = "empty";
+        var cell1 = row.insertCell(0);
+        cell1.id = "cells"
+        cell1.innerHTML = "Cart is empty";
+        document.getElementById("cells").setAttribute("colspan", 5);
+        /*update the html loopng thorugh each item in list  */
+    }
+    updateNumCart();
+}
+
+
+    function deleteRow(item) {
+        var cartItems = JSON.parse(sessionStorage.getItem("cart"));
+        var table = document.getElementsByClassName("itemsInCart")[0];
+        var row = item.parentNode.parentNode;
+        const index = row.rowIndex
+        if (index > -1){
+            cartItems.splice(index-1, 1);
+        }
+        row.parentNode.removeChild(row);
+        sessionStorage.setItem("cart", JSON.stringify(cartItems));
+        updateNumCart();
+            if (cartItems.length == 0){
+        var table = document.getElementsByClassName("itemsInCart")[0];
+     
+        var row = table.insertRow(1);
+        row.id = "empty";
+        var cell1 = row.insertCell(0);
+        cell1.id = "cells"
+        cell1.innerHTML = "Cart is empty";
+        document.getElementById("cells").setAttribute("colspan", 5);
+        /*update the html loopng thorugh each item in list  */
+    }
+}
+
+function updateNumCart(){
+    var cartItems = JSON.parse(sessionStorage.getItem("cart"));
+    document.getElementById('inc').value = cartItems.length;
+}
+
